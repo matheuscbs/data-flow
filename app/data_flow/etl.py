f2b32d995cb9ffb0226e0fe8aecc429147b9b3d3
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-06-18 17:22:33 -0300

    feat: improvements in etl resilience

--- app/data_flow/etl.py
+++ app/data_flow/etl.py
@@ -8,8 +8,6 @@ import time
 import schedule
 from kafka import KafkaProducer
 from kafka.errors import KafkaError, NoBrokersAvailable
-from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
 from pyspark.sql import SparkSession
 
 # Configuração do diretório e arquivo de log
@@ -23,42 +21,18 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # Constantes
 KAFKA_BROKER = "kafka:29092"
-TOPIC_NAME = "data-topic"
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "data_flow"
-COLLECTION_NAME = "state"
+TOPIC_NAME = "spark-etl-topic"
 os.environ['PYSPARK_PYTHON'] = "/usr/local/bin/python"
 os.environ['PYSPARK_DRIVER_PYTHON'] = "/usr/local/bin/python"
-
-# Conexão com o MongoDB
-client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-db = client[DB_NAME]
-state_collection = db[COLLECTION_NAME]
 
 # Criação do Spark Session e Kafka Producer fora do escopo das funções
 spark = SparkSession.builder.appName("Data Extraction and Kafka Example").getOrCreate()
 producer = None
 
-def load_last_id():
-    """ Carrega o último ID usado a partir do MongoDB. """
-    try:
-        state = state_collection.find_one({}, sort=[("timestamp", -1)])
-        return state['last_id'] if state else 0
-    except ServerSelectionTimeoutError as e:
-        logging.error(f"MongoDB connection failed: {e}")
-        sys.exit(1)
-
-def save_last_id(last_id):
-    """ Salva o último ID usado no MongoDB. """
-    try:
-        state_collection.insert_one({"last_id": last_id, "timestamp": time.time()})
-    except ServerSelectionTimeoutError as e:
-        logging.error(f"Failed to save state to MongoDB: {e}")
-
 def create_kafka_producer():
     global producer
-    max_attempts = 5
-    wait_time = 1
+    max_attempts = 10
+    wait_time = 10
     for attempt in range(max_attempts):
         try:
             producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
@@ -84,14 +58,8 @@ def send_data_to_kafka(data, topic):
             logging.error(f"Failed to send data to Kafka: {e}")
 
 def job():
-    last_id = load_last_id()
-    data = [
-        {"id": last_id + 1, "name": "Alice", "age": 30},
-        {"id": last_id + 2, "name": "Bob", "age": 25},
-        {"id": last_id + 3, "name": "Charlie", "age": 35}
-    ]
+    data = [{"id": 1, "name": "Alice", "age": 30}, {"id": 2, "name": "Bob", "age": 25}, {"id": 3, "name": "Charlie", "age": 35}]
     send_data_to_kafka(data, TOPIC_NAME)
-    save_last_id(data[-1]['id'])
 
 def graceful_exit(signal_num, frame):
     global producer
@@ -100,7 +68,6 @@ def graceful_exit(signal_num, frame):
         producer.close()
     logging.info("Stopping Spark session.")
     spark.stop()
-    client.close()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, graceful_exit)
